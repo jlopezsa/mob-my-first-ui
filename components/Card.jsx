92735dabbd98dbdef26f1c23ea486e33959fbc68
Julian Lopez <jlopezsa@gmail.com>
2022-05-07 02:31:38 -0500

    feat: Create list of data

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
-import { View, Text } from 'react-native-web'
+import React from "react";
+import { View, Text, StyleSheet, ImageBackground } from "react-native-web";
 
-const data = [
-    {
-        name: "Julian",
-        age: 38,
-    },
-    {
-        name: "Jair",
-        age: 30,
-    }
-]
-
-function Card() {
+function Card(props) {
   return (
-      <View>
-          <Text>Hello component</Text>
-          <Text>{data[1].name}</Text>
+    <View style={styles.container}>
+      <View style={styles.image}>
+        <ImageBackground source={{ uri: props.picture }}></ImageBackground>
       </View>
-  )
-}
+      <View style={styles.content}>
+        <Text>{props.about}</Text>
+        <Text>by {props.author}</Text>
+        <Text>
+          {props.tags.map((item) => {
+            return <Text key={item.name} style={styles.tag}>{item} </Text>;
+          })}
+        </Text>
+        <Text>{props.comments.length} comments</Text>
+      </View>
+    </View>
+  );
+};
 
-export default Card
+const styles = StyleSheet.create({
+  container: {
+    height: 250,
+    width: '100%',
+    flex: 1,
+    flexDirection: "row",
+  },
+  image: {
+    height: 270,
+    width: '30%',
+    backgroundColor: "blue",
+    marginTop: 10,
+  },
+  content: {
+    height: 270,
+    width: '70%',
+    backgroundColor: "red",
+    marginTop: 10,
+  },
+  tag: {
+    backgroundColor: "yellow",
+    width: 100,
+    margin: 5,
+  }
+});
+
+export default Card;
